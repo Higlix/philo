48697d221bb2fae0b2a045f7d6249c39aa202d6d
Khiglix <higlix@Khiglixs-MacBook-Pro.local>
2022-09-07 11:22:20 +0300

    philo_loop edit

--- philo/src/philo.c
+++ philo/src/philo.c
@@ -3,9 +3,19 @@
 void    *philo_loop_master(void *args)
 {
     t_data *data;
+    int index;
 
     data = (t_data *) args;
-
+    index = 0;
+    while (true) 
+    {
+        if (data->cycle_count == data->nop)
+            break ;
+        if (index == data->nop)
+            index = 0;
+        usleep(data->nop * 0.25 * 1000);
+        
+    }
 
     return (NULL);
 }
@@ -23,7 +33,16 @@ void    *philo_loop(void *args)
     }
     while (philo->data->running)
     {
-        
+        p_pick_fork(philo, get_time_ms());
+        p_eat(philo, get_time_ms());
+        p_drop_fork(philo, get_time_ms());
+        p_think(philo, get_time_ms());
+        if (philo->eat_count == philo->data->notepme)
+        {
+            philo->data->cycle_count++;
+            break ;
+        }
+        p_sleep(philo, get_time_ms());
     }
     
 
